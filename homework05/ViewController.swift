4efde84f5ef973cea5e6c095fab66a709d125028
sonic-business <sonicspeed19910623@gmail.com>
2020-12-10 16:43:08 +0800

    Initial Commit

---
+++ homework05/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework05
+//
+//  Created by student on 2020/12/10.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
